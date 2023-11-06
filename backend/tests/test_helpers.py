e3b1703a807025cd12384d7ed13fa1c67310feba
chefnoid <chefnoid@localhost.localdomain>
2023-11-06 11:45:44 +1100

    write new tests

--- backend/tests/test_helpers.py
+++ backend/tests/test_helpers.py
@@ -1,5 +1,5 @@
 from ..backend import helpers
-
+import pandas as pd
 
 def test_sanitize_date():
     dash_date = '15th September 2022 –14th October 2022'
@@ -11,3 +11,8 @@ def test_sanitize_date():
     assert clean1 == '15-Sep-2022 - 14-Oct-2022'
     assert clean2 == '15-Sep-2022 - 14-Oct-2022'
     assert clean3 == '15-Sep-2022 - 14-Oct-2022'
+def test_get_lat_long():
+    df = helpers.get_lat_long('Nairobi')
+    test_df = pd.Series({'lat': '-1.3026148499999999', 'lon': '36.82884201813725'})
+
+    assert df.equals(test_df)
